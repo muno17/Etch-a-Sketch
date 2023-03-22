f2424a2e5949874c29cc5ef56c8797e2e6b3c168
muno17 <munozd17@gmail.com>
2023-03-22 13:23:53 -0400

    add javascript to change gridDiv to white when mouse is hovered over

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ window.addEventListener('load', () => gridCreator())
 
 // create a 32x32 grid
 function gridCreator() {
-    for (let i = 0; i < 32; i++) {
+    for (let i = 0; i < 16; i++) {
         // create a div for each row
         let gridRow = document.createElement('div')
         gridRow.setAttribute('id', 'gridRow')
         gridContainer.appendChild(gridRow)
 
         // create 32 divs per row
-        for (let j = 0; j < 32; j++) {
+        for (let j = 0; j < 16; j++) {
             let gridDiv = document.createElement('div')
             gridDiv.setAttribute('id', 'gridDiv')
             gridRow.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', (gridDiv) => {
+                gridDiv.target.style.backgroundColor = 'white';
+            })
         }
 }}
