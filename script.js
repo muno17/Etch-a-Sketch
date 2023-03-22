fd55fa70f2305e06a382c26dc66a4da50d3f39fa
muno17 <munozd17@gmail.com>
2023-03-22 10:48:46 -0400

    add gridCreator() to javascript

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let gridContainer = document.getElementById('gridContainer');
-window.addEventListener('load', () => {
+window.addEventListener('load', () => gridCreator())
+
+
+
+
+
+// create a 16x16 grid
+function gridCreator() {
     console.log('loaded')
-    for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-        let gridDiv = document.createElement('div')
-        gridContainer.appendChild(gridDiv);
-        console.log('all the way in')
-    }
-}})
+    for (let i = 0; i < 16; i++) {
+        // create a div for each row
+        let gridRow = document.createElement('div')
+        gridContainer.appendChild(gridRow)
 
-
-
-// // create a 16x16 grid of divs
-// function gridCreator() {
-//     for (let i = 0; i < 4; i++) {
-//         for (let j = 0; j < 4; j++) {
-//             let gridDiv = document.createElement('div')
-//             gridContainer.appendChild(gridDiv)
-//         }
-//     }
-// }
+        // create 16 divs per row
+        for (let j = 0; j < 16; j++) {
+            let gridDiv = document.createElement('div')
+            gridRow.appendChild(gridDiv);
+            console.log('all the way in')
+        }
+}}
