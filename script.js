6fcdd17a84ae54afb82121fbe84c9660be8d9729
muno17 <munozd17@gmail.com>
2023-03-22 16:40:21 -0400

    add html and css for slider

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ window.addEventListener('load', () => gridCreator())
 
 // create a 32x32 grid
 function gridCreator() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 64; i++) {
         // create a div for each row
         let gridRow = document.createElement('div')
         gridRow.setAttribute('id', 'gridRow')
         gridContainer.appendChild(gridRow)
 
         // create 32 divs per row
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < 64; j++) {
             let gridDiv = document.createElement('div')
             gridDiv.setAttribute('id', 'gridDiv')
             gridRow.appendChild(gridDiv);
