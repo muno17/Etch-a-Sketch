c348788e21edf91a7098d014d93700dedaa7bb09
muno17 <munozd17@gmail.com>
2023-03-22 11:35:14 -0400

    add css to have rows appear in grid

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ window.addEventListener('load', () => gridCreator())
 
 // create a 16x16 grid
 function gridCreator() {
-    console.log('loaded')
     for (let i = 0; i < 16; i++) {
         // create a div for each row
         let gridRow = document.createElement('div')
+        gridRow.setAttribute('id', 'gridRow')
         gridContainer.appendChild(gridRow)
 
         // create 16 divs per row
         for (let j = 0; j < 16; j++) {
             let gridDiv = document.createElement('div')
+            gridDiv.setAttribute('id', 'gridDiv')
             gridRow.appendChild(gridDiv);
-            console.log('all the way in')
         }
 }}
